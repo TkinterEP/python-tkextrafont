7d626c3304d2d89234fafab5a6d13363fc516b17
RedFantom <redfantom@outlook.com>
2021-03-09 11:35:02 +0100

    Create custom target for copying Tcl files

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm tkextrafont/libextrafont.dll
 rm tkextrafont/libextrafont.so
+rm tkextrafont/*.tcl
 rm CMakeCache.txt
 rm cmake_install.cmake
 rm -rf cmake-build-debug cmake-build-release
