4fe10bb09de5711779680a65a8d93b6c0c47fcf7
RedFantom <redfantom@outlook.com>
2021-08-02 15:59:10 +0200

    Bump version number to v0.6.3

--- setup.py
+++ setup.py
@@ -179,7 +179,7 @@ else:
 
 setup(
     name="tkextrafont",
-    version="v0.6.2",
+    version="v0.6.3",
     packages=["tkextrafont"],
     description="Fonts loader for Tkinter",
     author="The extrafont and tkextrafont authors",
