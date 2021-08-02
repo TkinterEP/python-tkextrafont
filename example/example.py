351bb99ac6f2b23de2b07a8150d6e16b730c9755
RedFantom <redfantom@outlook.com>
2021-08-02 13:47:00 +0200

    Add example for Roboto

---
+++ example/example.py
@@ -0,0 +1,17 @@
+"""
+Author: RedFantom
+License: GNU GPLv3
+Copyright (c) 2018 RedFantom
+"""
+import tkinter as tk
+from tkinter import ttk
+from tkextrafont import Font
+
+window = tk.Tk()
+font = Font(file="Roboto-Medium.ttf")
+assert font.is_font_available("Roboto")
+assert "Roboto Medium" in font.loaded_fonts()
+assert font.font_info("Roboto-Medium.ttf")[0]["copyright"]
+ttk.Label(window, text="Roboto Font", font=("Roboto", 12)).grid()
+ttk.Label(window, text="Normal Font", font=("default", 12)).grid()
+window.mainloop()
